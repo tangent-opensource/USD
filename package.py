604ba6c04a8a440612d8cf1bf239e22e1a25adea
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-10-21 16:30:34 -0400

    Initial rez package for usd-19.11-houdini

---
+++ package.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+name = 'usd'
+
+version = '19.11-houdini-ta.1.0.0'
+
+authors = [
+    'benjamin.skinner',
+    'pixar',
+    'sidefx',
+]
+
+variants = [
+    ['platform-windows', 'arch-x64', 'os-windows-10', 'houdini-18.0.532'],
+]
+
+private_build_requires = [
+    'python-2',
+]
+
+# These could probably be broken down into more specific houdini versions
+# technically they are mismatched. But it works for now...
+requires = [
+    'zlib-1.2.11',
+    'tbb-2019.U9',
+    'boost-1.61.0-houdini',
+    'oiio-2.0.10-houdini',
+    'PyOpenGL-3.1.0'
+    #'~python-2'
+]
+
+# This package requires no building, only env setup
+build_command = 'python {root}/rez_build.py'
+
+def commands():
+    # Split and store version and package version
+    split_versions = str(version).split('-')
+    env.USD_VERSION.set(split_versions[0])
+    env.USD_PACKAGE_VERSION.set(split_versions[1])
+
+    env.USD_ROOT.append( '{root}' )
+
+    env.USD_INCLUDE_DIR.set( '{root}/include' )
+    env.USD_LIBRARY_DIR.set( '{root}/lib' )
+    env.USD_PYTHON_DIR.set( '{root}/lib/python' )
+
+
+    env.USD_HOUDINI.set("1")
---
+++ rez_build.py
@@ -0,0 +1,36 @@
+import os, subprocess, sys
+import shutil
+import glob
+
+# Eventually we might build directly from the Sidefx fork
+# https://github.com/sideeffects/USD
+# but for now we just copy to a rezified location
+
+if __name__ == "__main__":
+    src = os.environ["HOUDINI_ROOT"]
+    dst = os.environ["REZ_BUILD_INSTALL_PATH"]
+    inc_dst = os.environ["REZ_BUILD_INSTALL_PATH"] + "/include"
+    lib_dst = os.environ["REZ_BUILD_INSTALL_PATH"] + "/lib"
+
+    if 'win' in str(sys.platform):
+
+        # Remove existing build
+        if os.path.exists(dst):
+            print(" - Removing existing build")
+            shutil.rmtree(dst, ignore_errors=True)
+
+        print("Starting install")
+
+        shutil.copytree(src + "/toolkit/include/pxr", inc_dst + "/pxr")
+        
+        os.mkdir(lib_dst)
+
+        files = glob.glob(src + '/custom/houdini/dsolib/libpxr_*.lib')
+
+        for f in files:
+            #print(f)
+            shutil.copy(f, lib_dst + "/" + os.path.basename(f))
+
+        print("Install complete")
+
+        
