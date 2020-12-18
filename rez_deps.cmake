e5556d6d1eca94a4e59a49f9e9682921498718fc
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-12-18 15:10:55 -0500

    Fixed openvdb path

--- rez_deps.cmake
+++ rez_deps.cmake
@@ -54,6 +54,8 @@ file(TO_CMAKE_PATH $ENV{TBB_ROOT} TBB_ROOT_DIR)
 file(TO_CMAKE_PATH $ENV{OPENEXR_INCLUDE_DIR} OPENEXR_INCLUDE_DIR)
 file(TO_CMAKE_PATH $ENV{OPENEXR_ROOT} OPENEXR_LOCATION)
 
+file(TO_CMAKE_PATH $ENV{OPENVDB_INCLUDE_DIR} OPENVDB_INCLUDE_DIR)
+
 # These are both needed because \USD\pxr\imaging\glf\CMakeLists.txt doesnt include openexr headers when building with openvdb...
 # Noone else has Issued this on GH, so maybe our OpenVDB build shouldnt have exr? 
 set(OPENVDB_INCLUDE_DIR "${OPENVDB_INCLUDE_DIR};${OPENEXR_INCLUDE_DIR}")
