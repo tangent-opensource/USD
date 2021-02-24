1bc2482b04efedaa9ee65f61c433d6c88f952d8d
Charles Wardlaw <charles@skeletalstudios.com>
2021-02-24 13:59:54 -0500

    Build for maya and python-2.7

--- package.py
+++ package.py
@@ -2,43 +2,56 @@
 
 name = 'usd'
 
-version = '20.11-ta.1.0.0'
+version = '21.02-maya.2020.4-ta.1.1.0'
 
 authors = [
     'benjamin.skinner',
     'pixar',
+    'kiki',
 ]
 
+
+
 variants = [
-    ['platform-windows', 'arch-x64', 'os-windows-10'],
+    ['platform-windows', 'python-2'],
+    ['platform-windows', 'python-3'],
 ]
 
 requires = [
-    'python-3.7',
-    'PySide2-5.14.1',
-    'PyOpenGL-3.1.5',
+    'boost-1.70.0',
     'oiio-1.8.9',
     'ocio-1.1.1',
     'openvdb-7.0.0',
     'opensubdiv-3.4.3',
     'openexr-2.4.0',
-    'boost-1.69.0',
     'tbb-2019',
     'glew-2.0.0',
+    'zlib',
+    'PyOpenGL',     ## pip
+    'Jinja2',       ## pip
+    'MarkupSafe',   ## pip
 ]
+
+
+@early()
+def build_command():
+    import sys
+    if "win" in str(sys.platform):
+        return "{root}\\rez_build.ps1"
+    else:
+        raise NotImplementedError("OS not yet supported, sorry.")
+
 
 @early()
 def private_build_requires():
     import sys
     if 'win' in str(sys.platform):
-        return ['visual_studio']
+        return ['visual_studio', 'nasm']
     else:
         return ['gcc-7']
 
-build_system = "cmake"
 
 def commands():
-
     # Split and store version and package version
     split_versions = str(version).split('-')
     env.USD_VERSION.set(split_versions[0])
@@ -54,3 +67,4 @@ def commands():
     env.PATH.append( '{root}/lib' )
 
     env.PYTHONPATH.append( '{0}'.format( env.USD_PYTHON_DIR ) )
+
