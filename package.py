5aa3b98eacc95e0c886c27c8a526977b8101dcbd
Charles Wardlaw <charles@skeletalstudios.com>
2021-04-14 13:36:58 -0400

    This finally works with the floor rez build for boost.

--- package.py
+++ package.py
@@ -1,35 +1,34 @@
 # -*- coding: utf-8 -*-
 
-name = 'usd'
+name = "usd"
 
-version = '21.02-maya.2020.4-ta.1.1.0'
+version = "21.02-maya.2020.4-ta.1.2.0"
 
 authors = [
-    'benjamin.skinner',
-    'pixar',
-    'kiki',
+    "benjamin.skinner",
+    "pixar",
+    "kiki",
 ]
-
 
 
 variants = [
-    ['platform-windows', 'python-2'],
-    ['platform-windows', 'python-3'],
+    ["platform-windows", "python-2"],
+    # ["platform-windows", "python-3"],
 ]
 
 requires = [
-    'boost-1.70.0',
-    'oiio-1.8.9',
-    'ocio-1.1.1',
-    'openvdb-7.0.0',
-    'opensubdiv-3.4.3',
-    'openexr-2.4.0',
-    'tbb-2019',
-    'glew-2.0.0',
-    'zlib',
-    'PyOpenGL',     ## pip
-    'Jinja2',       ## pip
-    'MarkupSafe',   ## pip
+    "boost-1.70.0",
+    "oiio-1.8.9",
+    "ocio-1.1.1",
+    "openvdb-7.0.0",
+    "opensubdiv-3.4.3",
+    "openexr-2.4.0",
+    "tbb-2019",
+    "glew-2.0.0",
+    "zlib",
+    "PyOpenGL",     ## pip
+    "Jinja2",       ## pip
+    "MarkupSafe",   ## pip
 ]
 
 
@@ -45,26 +44,26 @@ def build_command():
 @early()
 def private_build_requires():
     import sys
-    if 'win' in str(sys.platform):
-        return ['visual_studio', 'nasm']
+    if "win" in str(sys.platform):
+        return ["visual_studio", "nasm"]
     else:
-        return ['gcc-7']
+        return ["gcc-7"]
 
 
 def commands():
     # Split and store version and package version
-    split_versions = str(version).split('-')
+    split_versions = str(version).split("-")
     env.USD_VERSION.set(split_versions[0])
     env.USD_PACKAGE_VERSION.set(split_versions[1])
 
-    env.USD_ROOT.append( '{root}' )
+    env.USD_ROOT.append("{root}")
 
-    env.USD_INCLUDE_DIR.set( '{root}/include' )
-    env.USD_LIBRARY_DIR.set( '{root}/lib' )
-    env.USD_PYTHON_DIR.set( '{root}/lib/python' )
+    env.USD_INCLUDE_DIR.set("{root}/include")
+    env.USD_LIBRARY_DIR.set("{root}/lib")
+    env.USD_PYTHON_DIR.set("{root}/lib/python")
     
-    env.PATH.append( '{root}/bin' )
-    env.PATH.append( '{root}/lib' )
+    env.PATH.append("{root}/bin")
+    env.PATH.append("{root}/lib")
 
-    env.PYTHONPATH.append( '{0}'.format( env.USD_PYTHON_DIR ) )
+    env.PYTHONPATH.append("{0}".format(env.USD_PYTHON_DIR))
 
--- rez_build.ps1
+++ rez_build.ps1
@@ -4,7 +4,7 @@ echo "USD Build: Installing to $env:REZ_BUILD_INSTALL_PATH"
 pushd $env:REZ_BUILD_SOURCE_PATH
 md build
 cd build
-cmake.exe -G "Visual Studio 15 2017 Win64" -Wno-dev --no-warn-unused-cli "-DCMAKE_INSTALL_PREFIX=$env:REZ_BUILD_INSTALL_PATH" "-DCMAKE_MODULE_PATH=$env:CMAKE_MODULE_PATH" -DCMAKE_BUILD_TYPE=Release -DREZ_BUILD_TYPE=local -DREZ_BUILD_INSTALL=1 "$env:REZ_BUILD_SOURCE_PATH" -DPXR_ENABLE_PYTHON_SUPPORT=OFF
+cmake -G "Visual Studio 15 2017 Win64" -Wno-dev --no-warn-unused-cli "-DCMAKE_INSTALL_PREFIX=$env:REZ_BUILD_INSTALL_PATH" "-DCMAKE_MODULE_PATH=$env:CMAKE_MODULE_PATH" -DCMAKE_BUILD_TYPE=Release -DREZ_BUILD_TYPE=local -DREZ_BUILD_INSTALL=1 "$env:REZ_BUILD_SOURCE_PATH" -DPXR_ENABLE_PYTHON_SUPPORT=ON
 cmake --build . --config Release --target INSTALL
 popd
 
